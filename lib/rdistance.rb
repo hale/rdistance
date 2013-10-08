d1936d7e6685acd897072785b6e2f1e2a98c5677
Philip Hale <phil989@gmail.com>
2013-10-09 00:08:19 +0100

    Base-case when either string is 0.

--- lib/rdistance.rb
+++ lib/rdistance.rb
@@ -1,5 +1,7 @@
 module RDistance
   def self.distance(s1, s2)
     return 0 if s1.length == s2.length
+    return s1.length if s2.empty?
+    return s2.length if s1.empty?
   end
 end
--- spec/rdistance_spec.rb
+++ spec/rdistance_spec.rb
@@ -4,4 +4,12 @@ describe RDistance do
   it "0 when the s1 == s2" do
     RDistance.distance("hello", "hello").should eq(0)
   end
+
+  it "length of s1 when s2 blank" do
+    RDistance.distance("wolf", "").should eq(4)
+  end
+
+  it "length of s2 when s1 blank" do
+    RDistance.distance("catamaran", "").should eq(9)
+  end
 end
